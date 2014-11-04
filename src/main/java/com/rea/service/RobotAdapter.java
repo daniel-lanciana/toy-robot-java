561188286fdd70374a7650c2219043eb7da37ac7
Daniel Lanciana <daniel.lanciana@gmail.com>
2014-11-05 00:16:12 +1100

    Adapter tests all pass

--- src/main/java/com/rea/service/RobotAdapter.java
+++ src/main/java/com/rea/service/RobotAdapter.java
@@ -1,15 +1,20 @@
 package com.rea.service;
 
+import com.rea.model.Direction;
 import com.rea.model.Robot;
+
+import java.util.Vector;
 
 /**
  * Created by akumadevil on 4/11/2014.
  */
 public class RobotAdapter {
+    private static String ERROR_MSG_INVALID_INPUT = "Invalid input";
+
     private Robot robot;
 
-    public void foo() {
-
+    public RobotAdapter() {
+        robot = new Robot();
     }
 
     public void setRobot(Robot robot) {
@@ -17,6 +22,45 @@ public class RobotAdapter {
     }
 
     public String input(String input) {
-        return null;
+        if (input != null) {
+            input = input.trim().toLowerCase();
+
+            switch(input) {
+                case "move":
+                    robot.move();
+                    break;
+                case "right":
+                    robot.right();
+                    break;
+                case "left":
+                    robot.left();
+                    break;
+                case "report":
+                    return robot.report();
+                default:
+                    if (isValidPlaceCommand(input)) {
+                        callPlaceCommand(input);
+                    }
+                    else {
+                        return ERROR_MSG_INVALID_INPUT;
+                    }
+            }
+        }
+
+        return "";
+    }
+
+    private void callPlaceCommand(String input) {
+        String[] args = input.replaceAll("place ", "").split(",");
+
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        Direction facing = Direction.valueOf(args[2].toUpperCase());
+
+        robot.place(x, y, facing);
+    }
+
+    private boolean isValidPlaceCommand(String input) {
+        return input.matches("^place [0-9]+,[0-9]+,(north|south|east|west)");
     }
 }
