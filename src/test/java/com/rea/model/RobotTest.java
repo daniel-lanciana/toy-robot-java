873a0ceaaed9f5c4ab0f0562f531b1ecdce5cbd1
Daniel Lanciana <daniel.lanciana@gmail.com>
2014-11-04 20:32:58 +1100

    Robot unit tests

--- src/test/java/com/rea/model/RobotTest.java
+++ src/test/java/com/rea/model/RobotTest.java
@@ -15,13 +15,13 @@ public class RobotTest {
     }
 
     @Test
-    public void shouldNotPlaceRobotInNegativePosition() throws Exception {
+    public void shouldNotPlaceInNegativePosition() throws Exception {
         robot.place(-1, -1, "north");
         assertEquals(robot.report, MSG_PLACE_FIRST);
     }
 
     @Test
-    public void shouldNotPlaceRobotOffTable() throws Exception {
+    public void shouldNotPlaceOffTable() throws Exception {
         robot.place(Table.LENGTH + 1, Table.LENGTH + 1, "north");
         assertEquals(robot.report, MSG_PLACE_FIRST);
     }
@@ -39,135 +39,140 @@ public class RobotTest {
     }
 
     @Test
-    public void shouldIgnoreReportIfNotPlaced() throws Exception {
+    public void shouldIgnoreIfNotPlaced() throws Exception {
         robot.report();
         assertEquals(robot.report, MSG_PLACE_FIRST);
     }
+
+    @Test
+    public void shouldPlaceOnTable() throws Exception {
+        robot.place(0, 0, "north");
+        assertEquals(robot.report, "0,0,NORTH");
+    }
+
+    @Test
+    public void shouldReplaceOnTable() throws Exception {
+        robot.place(0, 0, "north");
+        robot.place(3, 3, "south");
+        assertEquals(robot.report, "3,3,SOUTH");
+    }
+
+    @Test
+    public void shouldRotateLeft() throws Exception {
+        robot.place(0, 0, "north");
+        robot.left();
+        assertEquals(robot.report, "0,0,WEST");
+    }
+
+    @Test
+    public void shouldRotateLeftTwice() throws Exception {
+        robot.place(0, 0, "north");
+        robot.left();
+        robot.left();
+        assertEquals(robot.report, "0,0,SOUTH");
+    }
+
+    @Test
+    public void shouldRotateLeftThrice() throws Exception {
+        robot.place(0, 0, "north");
+        for (int i = 0; i < 3; i++) {
+            robot.left();
+        }
+        assertEquals(robot.report, "0,0,EAST");
+    }
+
+    @Test
+    public void shouldRotateLeftFullRotation() throws Exception {
+        robot.place(0, 0, "north");
+        for (int i = 0; i < 4; i++) {
+            robot.left();
+        }
+        assertEquals(robot.report, "0,0,NORTH");
+    }
+
+    @Test
+    public void shouldRotateRight() throws Exception {
+        robot.place(0, 0, "north");
+        robot.right();
+        assertEquals(robot.report, "0,0,EAST");
+    }
+
+    @Test
+    public void shouldRotateRightTwice() throws Exception {
+        robot.place(0, 0, "north");
+        robot.right();
+        robot.right();
+        assertEquals(robot.report, "0,0,SOUTH");
+    }
+
+    @Test
+    public void shouldRotateRightThrice() throws Exception {
+        robot.place(0, 0, "north");
+        for (int i = 0; i < 3; i++) {
+            robot.right();
+        }
+        assertEquals(robot.report, "0,0,WEST");
+    }
+
+    @Test
+    public void shouldRotateRightFullRotation() throws Exception {
+        robot.place(0, 0, "north");
+        for (int i = 0; i < 4; i++) {
+            robot.right();
+        }
+        assertEquals(robot.report, "0,0,NORTH");
+    }
+
+    @Test
+    public void shouldMoveOnce() throws Exception {
+        robot.place(0, 0, "north");
+        robot.move();
+        assertEquals(robot.report, "0,1,NORTH");
+    }
+
+    @Test
+    public void shouldMoveTwice() throws Exception {
+        robot.place(0, 0, "north");
+        robot.move();
+        robot.move();
+        assertEquals(robot.report, "0,2,NORTH");
+    }
+
+    @Test
+    public void shouldIgnoreMovingOffTable() throws Exception {
+        robot.place(0, 0, "north");
+        for (int i = 0; i < Table.LENGTH + 1; i++) {
+            robot.move();
+        }
+        assertEquals(robot.report, "0,4,NORTH");
+    }
+
+    @Test
+    public void shouldCombineMovingAndRotating() throws Exception {
+        robot.place(1, 2, "east");
+        robot.move();
+        robot.move();
+        robot.left();
+        robot.move();
+        assertEquals(robot.report, "3,3,NORTH");
+    }
+
+    @Test
+    public void shouldIgnoreMovingOffTableAllSides() throws Exception {
+        robot.place(0, 0, "north");
+
+        for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < Table.LENGTH; i++) {
+                robot.move();
+            }
+
+            robot.right();
+        }
+
+        robot.move();
+        robot.move();
+        robot.left();
+        robot.move();
+        assertEquals(robot.report, "0,0,NORTH");
+    }
 }
-
-/*
-
-
-  describe "placing the robot on the table" do
-    it "successful placement" do
-      @robot.place 0, 0, :north
-      @robot.report.must_equal "0,0,NORTH"
-    end
-
-    it "re-place robot" do
-      @robot.place 3, 3, :south
-      @robot.report.must_equal "3,3,SOUTH"
-    end
-  end
-
-  describe "rotating the robot" do
-    before do
-      @robot.place 0, 0, :north
-    end
-
-    it "rotate left once" do
-      @robot.left
-      @robot.report.must_equal "0,0,WEST"
-    end
-
-    it "rotate left twice" do
-      2.times do
-        @robot.left
-      end
-      @robot.report.must_equal "0,0,SOUTH"
-    end
-
-    it "rotate left thrice" do
-      3.times do
-        @robot.left
-      end
-      @robot.report.must_equal "0,0,EAST"
-    end
-
-    it "rotate left one full rotation" do
-      4.times do
-        @robot.left
-      end
-      @robot.report.must_equal "0,0,NORTH"
-    end
-
-    it "rotate right once" do
-      @robot.right
-      @robot.report.must_equal "0,0,EAST"
-    end
-
-    it "rotate right twice" do
-      2.times do
-        @robot.right
-      end
-      @robot.report.must_equal "0,0,SOUTH"
-    end
-
-    it "rotate right thrice" do
-      3.times do
-        @robot.right
-      end
-      @robot.report.must_equal "0,0,WEST"
-    end
-
-    it "rotate right one full rotation" do
-      4.times do
-        @robot.right
-      end
-      @robot.report.must_equal "0,0,NORTH"
-    end
-  end
-
-  describe "moving the robot forward" do
-    before do
-      @robot.place 0, 0, :north
-    end
-
-    it "forward one block" do
-      @robot.move
-      @robot.report.must_equal "0,1,NORTH"
-    end
-
-    it "forward two block" do
-      2.times do
-        @robot.move
-      end
-      @robot.report.must_equal "0,2,NORTH"
-    end
-
-    it "moving off the table is ignored" do
-      (Table.length + 1).times do
-        @robot.move
-      end
-      @robot.report.must_equal "0,4,NORTH"
-    end
-  end
-
-  describe "combined moving and rotating the robot" do
-    it "all over the place" do
-      @robot.place 1, 2, :east
-
-      2.times do
-        @robot.move
-      end
-
-      @robot.left
-      @robot.move
-      @robot.report.must_equal "3,3,NORTH"
-    end
-
-    it "can't be moved off the table on any side (assuming table is a square)" do
-      @robot.place 0, 0, :north
-
-      4.times do
-        (Table.length + 1).times do
-          @robot.move
-        end
-
-        @robot.right
-      end
-
-      @robot.report.must_equal "0,0,NORTH"
-    end
-  end
- */
